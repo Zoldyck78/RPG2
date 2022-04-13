f53e1177cdc62b4cc505dcd873ff47a540a3f411
SatganzDevs <94371633+SATGANZBOTZ@users.noreply.github.com>
2022-04-13 16:07:39 +0700

    Satganz Devs

---
+++ plugins/toxicc.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+let buff = fs.readFileSync('./media/Satzz.opus')}
+conn.sendFile(m.chat, buff, null, m, /vn/.test(args[0]), { quoted: m, mimetype: 'audio/mp4' })
+}
+
+handler.customPrefix = /an(j|g)|anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole|as(u|w)|tolol/i
+handler.command = new RegExp
+
+module.exports = handler
