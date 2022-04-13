2e3deb33c39ebf4209e4febc1db01ad4fc63a62a
SatganzDevs <94371633+SATGANZBOTZ@users.noreply.github.com>
2022-04-13 15:51:19 +0700

    Satganz Devs

--- plugins/toxicc.js
+++ plugins/toxicc.js
@@ -1,6 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-m.reply(`“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`)
+  
+let res = fs.readFileSync('./media/Satzz.opus')}
+conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
 }
 
 handler.customPrefix = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i
