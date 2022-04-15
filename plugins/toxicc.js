8a64655e6b3d9eb216b85f4685aa6e073bd1245d
SatganzDevs <94371633+SATGANZBOTZ@users.noreply.github.com>
2022-04-15 11:11:03 +0700

    Satganz Devs

--- plugins/toxicc.js
+++ plugins/toxicc.js
@@ -1,10 +1,12 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-let buff = fs.readFileSync('./media/Satzz.mp3')}
-conn.sendFile(m.chat, buff, 'tts.opus', null, m, true)
+m.reply(`
+عَنْ أَبِي الدَّرْدَاءِ، أَنَّ النَّبِيَّ صَلَّى اللَّهُ عَلَيْهِ وَسَلَّمَ قَالَ: إِنَّ اللَّهَ لَيُبْغِضُ الفَاحِشَ البَذِيءَ
+
+Dari Abu Ad-Darda’ radhiallahu ‘anhu bahwasanya Rasulullah ﷺ bersabda, “Sungguh Allah benci dengan orang yang lisannya kotor dan kasar.”
 }
 
-handler.customPrefix = /an(j|g)|anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole|as(u|w)|tolol/i
+handler.customPrefix = /an(j|g)|anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole|as(u|w)|olol/i
 handler.command = new RegExp
 
 module.exports = handler
